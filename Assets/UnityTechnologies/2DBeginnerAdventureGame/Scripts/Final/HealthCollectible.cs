31a47bf16322d3ef4ec8a95c60e8861e6dc575c9
PandeeDev <mayolchris@gmail.com>
2024-11-17 23:17:50 -0500

    Github Desktop Push
    Github Push Test from Desktop

---
+++ Assets/UnityTechnologies/2DBeginnerAdventureGame/Scripts/Final/HealthCollectible.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Will handle giving health to the character when they enter the trigger.
+/// </summary>
+public class HealthCollectible : MonoBehaviour 
+{
+    public ParticleSystem healthParticle;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            Instantiate(healthParticle, transform.position + Vector3.up * 0.5f, Quaternion.identity);
+            controller.ChangeHealth(1);
+            Destroy(gameObject);
+        }
+    }
+}
